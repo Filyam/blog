1d3da35d52c85ed3ffbe7332615585a60bf1b7b6
Филип <filyamintotheface@gmail.com>
2020-06-14 17:48:46 +0300

    add headr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { HeaderMenuComponent } from './Components/header-menu/header-menu.component';
+import { HeaderSearchInputComponent } from './Components/header-search-input/header-search-input.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PagesTopdownComponent } from './Components/pages-topdown/pages-topdown.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HeaderSocialcontactsComponent } from './Components/header-socialcontacts/header-socialcontacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeaderMenuComponent,
+    HeaderSearchInputComponent,
+    PagesTopdownComponent,
+    HeaderSocialcontactsComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        FontAwesomeModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+  }
+}
